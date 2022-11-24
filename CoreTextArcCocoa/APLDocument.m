f2bc95c1245fd99725c5b6003bbae3820df99d88
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2022-11-23 21:48:07 -0500

    cleanup dup code

--- CoreTextArcCocoa/APLDocument.m
+++ CoreTextArcCocoa/APLDocument.m
@@ -1,48 +1,48 @@
 /*
-     File: APLDocument.m 
- Abstract: n/a 
-  Version: 1.2 
-  
- Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple 
- Inc. ("Apple") in consideration of your agreement to the following 
- terms, and your use, installation, modification or redistribution of 
- this Apple software constitutes acceptance of these terms.  If you do 
- not agree with these terms, please do not use, install, modify or 
- redistribute this Apple software. 
-  
- In consideration of your agreement to abide by the following terms, and 
- subject to these terms, Apple grants you a personal, non-exclusive 
- license, under Apple's copyrights in this original Apple software (the 
- "Apple Software"), to use, reproduce, modify and redistribute the Apple 
- Software, with or without modifications, in source and/or binary forms; 
- provided that if you redistribute the Apple Software in its entirety and 
- without modifications, you must retain this notice and the following 
- text and disclaimers in all such redistributions of the Apple Software. 
- Neither the name, trademarks, service marks or logos of Apple Inc. may 
- be used to endorse or promote products derived from the Apple Software 
- without specific prior written permission from Apple.  Except as 
- expressly stated in this notice, no other rights or licenses, express or 
- implied, are granted by Apple herein, including but not limited to any 
- patent rights that may be infringed by your derivative works or by other 
- works in which the Apple Software may be incorporated. 
-  
- The Apple Software is provided by Apple on an "AS IS" basis.  APPLE 
- MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION 
- THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS 
- FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND 
- OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS. 
-  
- IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL 
- OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, 
- MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED 
- AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), 
- STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE 
- POSSIBILITY OF SUCH DAMAGE. 
-  
- Copyright (C) 2013 Apple Inc. All Rights Reserved. 
-  
+ File: APLDocument.m
+ Abstract: n/a
+ Version: 1.2
+ 
+ Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
+ Inc. ("Apple") in consideration of your agreement to the following
+ terms, and your use, installation, modification or redistribution of
+ this Apple software constitutes acceptance of these terms.  If you do
+ not agree with these terms, please do not use, install, modify or
+ redistribute this Apple software.
+ 
+ In consideration of your agreement to abide by the following terms, and
+ subject to these terms, Apple grants you a personal, non-exclusive
+ license, under Apple's copyrights in this original Apple software (the
+ "Apple Software"), to use, reproduce, modify and redistribute the Apple
+ Software, with or without modifications, in source and/or binary forms;
+ provided that if you redistribute the Apple Software in its entirety and
+ without modifications, you must retain this notice and the following
+ text and disclaimers in all such redistributions of the Apple Software.
+ Neither the name, trademarks, service marks or logos of Apple Inc. may
+ be used to endorse or promote products derived from the Apple Software
+ without specific prior written permission from Apple.  Except as
+ expressly stated in this notice, no other rights or licenses, express or
+ implied, are granted by Apple herein, including but not limited to any
+ patent rights that may be infringed by your derivative works or by other
+ works in which the Apple Software may be incorporated.
+ 
+ The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
+ MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
+ OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+ 
+ IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+ MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
+ AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+ STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ 
+ Copyright (C) 2013 Apple Inc. All Rights Reserved.
+ 
  */
 
 #import "APLDocument.h"
@@ -71,91 +71,69 @@
 @implementation APLDocument
 
 - (IBAction)setString:(id)sender {
-	self.arcView.string = [sender stringValue];
-	[self updateDisplay];
+    self.arcView.string = [sender stringValue];
+    [self updateDisplay];
 }
 
 
 - (IBAction)setShowsGlyphOutlines:(id)sender {
     self.arcView.showsGlyphBounds = ([sender state] == NSControlStateValueOn);
-	[self updateDisplay];
+    [self updateDisplay];
 }
 
 
 - (IBAction)setShowsLineMetrics:(id)sender {
     self.arcView.showsLineMetrics = ([sender state] == NSControlStateValueOn);
-	[self updateDisplay];
+    [self updateDisplay];
 }
 
 
 - (IBAction)setDimsSubstitutedGlyphs:(id)sender {
     self.arcView.dimsSubstitutedGlyphs = ([sender state] == NSControlStateValueOn);
-	[self updateDisplay];
+    [self updateDisplay];
 }
 
 
 #pragma mark - Managing font
 
 - (void)setArcViewFont:(NSFont *)newFont {
-	if (newFont != nil) {
-		self.arcView.font = newFont;
-		[self updateDisplay];
-		[[NSFontPanel sharedFontPanel] setPanelFont:self.arcView.font isMultiple:NO];
-	}
+    if (newFont != nil) {
+        self.arcView.font = newFont;
+        [self updateDisplay];
+        [[NSFontPanel sharedFontPanel] setPanelFont:self.arcView.font isMultiple:NO];
+    }
 }
 
 /*
  The logic of toggling state from a button is the opposite of that from a menu item.
  */
 - (IBAction)takeBoldSettingFromButton:(NSButton *)sender {
-    if ([sender state] == NSControlStateValueOn) {
-		[self changeArcViewFontTrait:NSFontBoldTrait to:YES];
-	}
-	else {
-		[self changeArcViewFontTrait:NSFontBoldTrait to:NO];
-	}
+    [self changeArcViewFontTrait:NSFontBoldTrait to:[sender state] == NSControlStateValueOn];
 }
 
 
 - (IBAction)takeBoldSettingFrom:(id)sender {
-    if ([sender state] == NSControlStateValueOff) {
-		[self changeArcViewFontTrait:NSFontBoldTrait to:YES];
-	}
-	else {
-		[self changeArcViewFontTrait:NSFontBoldTrait to:NO];
-	}
+    [self changeArcViewFontTrait:NSFontBoldTrait to:[sender state] == NSControlStateValueOff];
 }
 
 
 - (IBAction)takeItalicSettingFromButton:(NSButton *)sender {
-    if ([sender state] == NSControlStateValueOn) {
-		[self changeArcViewFontTrait:NSFontItalicTrait to:YES];
-	}
-	else {
-		[self changeArcViewFontTrait:NSFontItalicTrait to:NO];
-	}
+    [self changeArcViewFontTrait:NSFontItalicTrait to:[sender state] == NSControlStateValueOn];
 }
 
 
 - (IBAction)takeItalicSettingFrom:(id)sender {
-    if ([sender state] == NSControlStateValueOff) {
-		[self changeArcViewFontTrait:NSFontItalicTrait to:YES];
-	}
-	else {
-		[self changeArcViewFontTrait:NSFontItalicTrait to:NO];
-	}
+    [self changeArcViewFontTrait:NSFontItalicTrait to:[sender state] == NSControlStateValueOff];
 }
 
 
-- (void)changeArcViewFontTrait:(NSInteger)fontTrait to:(BOOL)yn{
-
-	NSFont *newFont;
-	if (yn) {
-		newFont = [[NSFontManager sharedFontManager] convertFont:self.arcView.font toHaveTrait:fontTrait];
-	}
-	else {
-		newFont = [[NSFontManager sharedFontManager] convertFont:self.arcView.font toNotHaveTrait:fontTrait];
-	}
+- (void)changeArcViewFontTrait:(NSInteger)fontTrait to:(BOOL)yn {
+    
+    NSFont *const newFont =
+    yn
+    ? [[NSFontManager sharedFontManager] convertFont:self.arcView.font toHaveTrait:fontTrait]
+    : [[NSFontManager sharedFontManager] convertFont:self.arcView.font toNotHaveTrait:fontTrait];
+    
     [self setArcViewFont:newFont];
 }
 
@@ -175,89 +153,58 @@
 #pragma mark - Managing UI
 
 - (void)updateDisplay {
-	[self.arcView setNeedsDisplay:YES];
-
-	// Update the bold button.
-    if ([self.arcView.font isBold]) {
-        [self.boldButton setState:NSControlStateValueOn];
-    }
-    else {
-        [self.boldButton setState:NSControlStateValueOff];
-    }
-
-    if ([self.arcView.font canToggleTrait:NSFontBoldTrait]) {
-        [self.boldButton setEnabled:YES];
-    }
-    else {
-        [self.boldButton setEnabled:NO];
-    }
+    [self.arcView setNeedsDisplay:YES];
     
+    // Update the bold button.
+    [self.boldButton setState:[self.arcView.font isBold]];
     
-	// Update the italic button.
-    if ([self.arcView.font isItalic]) {
-        [self.italicButton setState:NSControlStateValueOn];
-    }
-    else {
-        [self.italicButton setState:NSControlStateValueOff];
-    }
-
-    if ([self.arcView.font canToggleTrait:NSFontItalicTrait]) {
-        [self.italicButton setEnabled:YES];
-    }
-    else {
-        [self.italicButton setEnabled:NO];
-    }
+    [self.boldButton setEnabled:[self.arcView.font canToggleTrait:NSFontBoldTrait]];
     
-	// Update the window title.
-	for (NSWindowController *controller in [self windowControllers]) {
-		[[controller window] setTitle:[self displayName]];
-	}
+    // Update the italic button.
+    [self.italicButton setState:[self.arcView.font isItalic]];
+    
+    [self.italicButton setEnabled:[self.arcView.font canToggleTrait:NSFontItalicTrait]];
+    
+    // Update the window title.
+    for (NSWindowController *controller in [self windowControllers]) {
+        [[controller window] setTitle:[self displayName]];
+    }
 }
 
 
 - (BOOL)validateUserInterfaceItem:(id <NSValidatedUserInterfaceItem>)item {
-	SEL action = [item action];
-
-	if (sel_isEqual(action, @selector(takeBoldSettingFrom:))) {
-		// Set the state based on the presence of the trait.
-		if ([(id)item respondsToSelector:@selector(setState:)]) {
-            if ([self.arcView.font isBold]) {
-                [(id)item setState:NSControlStateValueOn];
-            }
-            else {
-                [(id)item setState:NSControlStateValueOff];
-            }
-		}
-		// Test whether we can convert the traits to enable or disable the control.
-		return [self.arcView.font canToggleTrait:NSFontBoldTrait];
-	}
-	else if (sel_isEqual(action, @selector(takeItalicSettingFrom:))) {
-		// Set the state based on the presence of the trait.
-		if ([(id)item respondsToSelector:@selector(setState:)]) {
-            if ([self.arcView.font isItalic]) {
-                [(id)item setState:NSControlStateValueOn];
-            }
-            else {
-                [(id)item setState:NSControlStateValueOff];
-            }
-		}
-		// Test whether we can convert the traits to enable or disable the control.
-		return [self.arcView.font canToggleTrait:NSFontItalicTrait];
-	}
-	return YES;
+    SEL action = [item action];
+    
+    if (sel_isEqual(action, @selector(takeBoldSettingFrom:))) {
+        // Set the state based on the presence of the trait.
+        if ([(id)item respondsToSelector:@selector(setState:)]) {
+            [(id)item setState:[self.arcView.font isBold]];
+        }
+        // Test whether we can convert the traits to enable or disable the control.
+        return [self.arcView.font canToggleTrait:NSFontBoldTrait];
+    }
+    else if (sel_isEqual(action, @selector(takeItalicSettingFrom:))) {
+        // Set the state based on the presence of the trait.
+        if ([(id)item respondsToSelector:@selector(setState:)]) {
+            [(id)item setState:[self.arcView.font isItalic]];
+        }
+        // Test whether we can convert the traits to enable or disable the control.
+        return [self.arcView.font canToggleTrait:NSFontItalicTrait];
+    }
+    return YES;
 }
 
 
 - (void)windowDidBecomeKey:(NSNotification *)notification {
-	[[NSFontManager sharedFontManager] setTarget:self];
-	[[NSFontPanel sharedFontPanel] setPanelFont:self.arcView.font isMultiple:NO];
+    [[NSFontManager sharedFontManager] setTarget:self];
+    [[NSFontPanel sharedFontPanel] setPanelFont:self.arcView.font isMultiple:NO];
 }
 
 
 #pragma - NSDocument overrides
 
 - (NSString *)displayName {
-	return self.arcView.string;
+    return self.arcView.string;
 }
 
 
@@ -276,30 +223,27 @@
 @implementation NSFont (FontStyleAdditions)
 
 - (BOOL)isBold {
-	return ([[self fontDescriptor] symbolicTraits] & NSFontBoldTrait);
+    return ([[self fontDescriptor] symbolicTraits] & NSFontBoldTrait);
 }
 
 
 - (BOOL)isItalic {
-	return ([[self fontDescriptor] symbolicTraits] & NSFontItalicTrait);
+    return ([[self fontDescriptor] symbolicTraits] & NSFontItalicTrait);
 }
 
 
 - (BOOL)canToggleTrait:(NSFontTraitMask)trait {
-	NSFont *testFont = nil;
-	if ([[self fontDescriptor] symbolicTraits] & trait) {
-		testFont = [[NSFontManager sharedFontManager] convertFont:self toNotHaveTrait:trait];
-	}
-	else {
-		testFont = [[NSFontManager sharedFontManager] convertFont:self toHaveTrait:trait];
-	}
-
-	if (testFont != nil) {
-		if (([[testFont fontDescriptor] symbolicTraits] ^ [[self fontDescriptor] symbolicTraits]) == trait) {
-			return YES;
-		}
-	}
-	return NO;
+    NSFont *const testFont =
+    ([[self fontDescriptor] symbolicTraits] & trait)
+    ? [[NSFontManager sharedFontManager] convertFont:self toNotHaveTrait:trait]
+    : [[NSFontManager sharedFontManager] convertFont:self toHaveTrait:trait];
+    
+    if (testFont != nil) {
+        if (([[testFont fontDescriptor] symbolicTraits] ^ [[self fontDescriptor] symbolicTraits]) == trait) {
+            return YES;
+        }
+    }
+    return NO;
 }
 
 @end
